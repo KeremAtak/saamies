80c9d7ccf0959dad4d6e15f916241573bea30689
KeremAtak <kerem.atak@hotmail.com>
2019-01-21 05:35:13 +0200

    Now forecast is based on browser location

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,10 @@
-/* const debug = require('debug')('weathermap');
- */
 const Koa = require('koa');
 const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
-
 // This should obviously be hidden in a real project
 const appId = process.env.APPID || '50b56dbdeeb900d43f4ba09cf7646056';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -16,18 +12,22 @@ const app = new Koa();
 
 app.use(cors());
 
-const fetchForecast = async () => {
-  const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
+//Fetches data with given latitude and longitude
+const fetchForecast = async (lat, lon) => {
+  const endpoint = `${mapURI}/forecast?lat=${lat}&lon=${lon}&appid=${appId}&`;
   const response = await fetch(endpoint);
-
+  
   return response ? response.json() : {};
 };
 
-router.get('/api/forecast', async ctx => {
-  const forecastData = await fetchForecast();
+//Returns extensive forecast data
+router.get('/api/forecast', async (ctx) => {
+  const lat = ctx.query.lat
+  const lon = ctx.query.lon
+  const forecastData = await fetchForecast(lat, lon);
 
   ctx.type = 'application/json; charset=utf-8';
-  ctx.body = forecastData.list ? forecastData.list[3] : {};
+  ctx.body = forecastData.list ? forecastData.list[2] : {};
 });
 
 app.use(router.routes());
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 
 const baseURL = process.env.ENDPOINT;
 
-const getWeatherFromApi = async () => {
+// Fetches approximately 6-hour forecast from api by using coordinates
+const getForecastFromApi = async (lat, lon) => {
   try {
-    const response = await fetch(`${baseURL}/forecast`);
+    const response = await fetch(`${baseURL}/forecast?lat=${lat}&lon=${lon}`);
     return response.json();
   } catch (error) {
     console.error(error);
   }
-
   return {};
 };
 
@@ -24,7 +24,25 @@ class Weather extends React.Component {
   }
 
   async componentWillMount() {
-    const forecastData = await getWeatherFromApi();
+    this.getPosition();
+    // Must be called on some coordinates at first due to async nature
+    this.changeIcon(0, 0);
+  }
+
+  // Fetches the coordinates from the browser once given permissions
+  async getPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      this.changeIcon(lat, lon);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  // Fetches appropriate data to have the image changed
+  async changeIcon(lat, lon) {
+    const forecastData = await getForecastFromApi(lat, lon);
     const weather = forecastData.weather;
     this.setState({ icon: weather[0].icon.slice(0, -1) });
   }
